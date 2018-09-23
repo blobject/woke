18f9cbd0a9950bceb238c95b205779996827d133
agaric <agaric@protonmail.com>
2018-09-23 15:47:22 +0200

    update domain

--- component/Foot.js
+++ component/Foot.js
@@ -9,11 +9,11 @@ const Foot = () => (
       <span className="bold">keys:</span> (<span className="bolder">Shift</span>)<span className="bolder">Tab</span> cycle focus, <span className="bolder">Up</span>/<span className="bolder">Down</span> +1/-1, <span className="bolder">Enter</span> start/pause, <span className="bolder">Space</span> pause, <span className="bolder">Escape</span> stop
     </div>
     <div className="right">
-      <a href="https://github.com/agarick/woke">
+      <a href="https://github.com/blobject/woke">
         <img src={prefix("/pub/img/github.png")} />
       </a>
       {" "}brought to you by
-      {" "}<a href="https://agaric.net">agaric</a>
+      {" "}<a href="https://b.agaric.net">agaric</a>
     </div>
   </div>
 )
